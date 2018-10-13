6a401126d9d48f30a88aa90502f2fcb2a26d05d6
Tarlan <tarlanru@code.edu.az>
2018-10-13 12:37:51 +0300

    FrontChangedForMuradiagziduzelsin

---
+++ BlogezyTeamWork/BlogezyTeamWork/Models/UserAndRoleCreater.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogezyTeamWork.Models
+{
+    public class UserAndRoleCreater
+    {
+        public static async Task CreateAsync(IServiceScope scope,IdentityDbContext<AppUser> db)
+        {
+            if (!db.Users.Any() && !db.Roles.Any())
+            {
+
+                UserManager<AppUser> userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+                RoleManager<IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                AppUser user = new AppUser()
+                {
+                    Email = "admin@gmail.com"
+                };
+                var identityResult =  await userManager.CreateAsync(user, "Admin");
+                if (identityResult.Succeeded)
+                {
+                    await roleManager.CreateAsync(new IdentityRole() { Name = "Admin"});
+                    await roleManager.CreateAsync(new IdentityRole() { Name = "User" });
+                    await userManager.AddToRoleAsync(user,"Admin");
+                }
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
--- BlogezyTeamWork/BlogezyTeamWork/Program.cs
+++ BlogezyTeamWork/BlogezyTeamWork/Program.cs
@@ -125,6 +125,8 @@ namespace BlogezyTeamWork
 
 
                     }
+
+                    //UserAndRoleCreater.CreateAsync(scopedService, dbContext).Wait();
                 }
             }
 
